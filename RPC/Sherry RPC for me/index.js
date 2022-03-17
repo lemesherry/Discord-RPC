7b61cf0d562baa64ab8cf4a14d333582d1582660
Sherry <sghoto7@gmail.com>
2022-03-16 23:05:40 -0700

    updated code

--- RPC/Sherry RPC for me/index.js
+++ RPC/Sherry RPC for me/index.js
@@ -1,15 +1,18 @@
 // created by Sherry //
 const DiscordRPC = require('discord-rpc'); // to use discord rich presence modules in this code.
+const clientID = "853261276738486333";
 const client = new DiscordRPC.Client({ transport: 'ipc' }); // making client variable with in our rich presence client.
 require('dotenv').config(); // to include contents of our .env file (the application id).
+DiscordRPC.register(clientID);
+
 (async () => {
     client.on('ready', async () => { // starting ready event.
         await client.setActivity({ // Set the values as you want, these values will appear on your status.
-// NOTE: be carefull only change the value that are written inside double quoted commas.
+            // NOTE: be carefull only change the value that are written inside double quoted commas.
             buttons: [
-            { label: `don't click (explicit content)`, url: "https://www.instagram.com/leme_sherry" },
-        { label: `BOT LINK`, url: "https://discord.com/oauth2/authorize?client_id=845208558736113684&permissions=37088600&scope=bot" }],
-//        {label: "Github", url: "https://github.com/lemesherry"}],
+                { label: `don't click (explicit content)`, url: "https://www.pinterest.com/lemesherry" },
+                // { label: `BOT LINK`, url: "https://discord.com/oauth2/authorize?client_id=845208558736113684&permissions=37088600&scope=bot" }],
+                { label: "Github", url: "https://github.com/lemesherry" }],
             details: "Stop stalking babes ^_^",
             state: "Love you 3000 now fuck off <3",
             startTimestamp: new Date(),
@@ -22,5 +25,5 @@ require('dotenv').config(); // to include contents of our .env file (the applica
         console.log("Discord Rich Presence has been enabled.");
     });
 
-    await client.login({ clientId: process.env.applicationID }).catch(console.error); // Logging into our application.
+    await client.login({ clientId: clientID }).catch(console.error); // Logging into our application.
 })();
