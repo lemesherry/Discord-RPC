f4ea354da3b543cba77a96de1427dfc00f66e0af
Sherry <84338798+lemesherry@users.noreply.github.com>
2022-03-20 00:35:17 +0500

    updated

--- RPC/Sherry RPC AFK/index.js
+++ RPC/Sherry RPC AFK/index.js
@@ -1,16 +1,18 @@
 // created by Sherry //
-const DiscordRPC = require('discord-rpc'); // to use discord rich presence modules in this code.
-const client = new DiscordRPC.Client({ transport: 'ipc' }); // making client variable with in our rich presence client.
-require('dotenv').config(); // to include contents of our .env file (the application id).
+const DiscordRPC = require('discord-rpc'); // to use discord rich presence module in this code.
+const clientID = "875035427977953330"; // client id of application
+const client = new DiscordRPC.Client({ transport: 'ipc' }); // calling client constructor in order to access its variables and functions
+DiscordRPC.register(clientID); // registering client id
+
 (async () => {
-    client.on('ready', async () => { // starting ready event.
-        await client.setActivity({ // Set the values as you want, these values will appear on your status.
-// NOTE: be carefull only change the value that are written inside double quoted commas.
+    client.on('ready', async () => { // performing functionality on application ready.
+        await client.setActivity({ // Activity settings
+            /* NOTE: be carefull only change the value that are written string values.
+             Set the values as you want, these values will appear on your status.
+            */
             buttons: [
-            { label: `Don't Click (Explicit Content)`, url: "https://www.instagram.com/leme_sherry" }
-//            ,
-//        {label: "My GitHub", url: "https://github.com/lemesherry"}
-            ],
+                { label: `don't click (explicit content)`, url: "https://www.pinterest.com/lemesherry" },
+                { label: "Github", url: "https://github.com/lemesherry" }],
             details: "Check on me later 👀",
             state: "stop stalking now honey",
             startTimestamp: new Date(),
@@ -23,5 +25,5 @@ require('dotenv').config(); // to include contents of our .env file (the applica
         console.log("Discord Rich Presence has been enabled.");
     });
 
-    await client.login({ clientId: process.env.applicationID }).catch(console.error); // Logging into our application.
+    await client.login({ clientId: clientID }).catch(console.error); // Logging into our application.
 })();
