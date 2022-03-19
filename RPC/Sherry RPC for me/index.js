7e164c8016f57f28039cab66d4ff202a1580d775
Sherry <84338798+lemesherry@users.noreply.github.com>
2022-03-20 00:32:23 +0500

    updated

--- RPC/Sherry RPC for me/index.js
+++ RPC/Sherry RPC for me/index.js
@@ -1,17 +1,17 @@
 // created by Sherry //
-const DiscordRPC = require('discord-rpc'); // to use discord rich presence modules in this code.
-const clientID = "853261276738486333";
-const client = new DiscordRPC.Client({ transport: 'ipc' }); // making client variable with in our rich presence client.
-require('dotenv').config(); // to include contents of our .env file (the application id).
-DiscordRPC.register(clientID);
+const DiscordRPC = require('discord-rpc'); // to use discord rich presence module in this code.
+const clientID = "853261276738486333"; // client id of application
+const client = new DiscordRPC.Client({ transport: 'ipc' }); // calling client constructor in order to access its variables and functions
+DiscordRPC.register(clientID); // registering client id
 
 (async () => {
-    client.on('ready', async () => { // starting ready event.
-        await client.setActivity({ // Set the values as you want, these values will appear on your status.
-            // NOTE: be carefull only change the value that are written inside double quoted commas.
+    client.on('ready', async () => { // performing functionality on application ready.
+        await client.setActivity({ // Activity settings
+        /* NOTE: be carefull only change the value that are written string values.
+         Set the values as you want, these values will appear on your status.
+        */
             buttons: [
                 { label: `don't click (explicit content)`, url: "https://www.pinterest.com/lemesherry" },
-                // { label: `BOT LINK`, url: "https://discord.com/oauth2/authorize?client_id=845208558736113684&permissions=37088600&scope=bot" }],
                 { label: "Github", url: "https://github.com/lemesherry" }],
             details: "Stop stalking babes ^_^",
             state: "Love you 3000 now fuck off <3",
